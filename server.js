18a2b927d89380b4cf3af8bf211e23b3d1f0e4c6
Kehinde John Omotoso <kehindejohnomotoso@gmail.com>
2025-06-05 13:48:11 +0100

    Update from Kehinde

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.use('*', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`CRUD Project Server is running on port ${PORT}`);
   console.log(`API Documentation: http://localhost:${PORT}/api-docs`);
 }); 
